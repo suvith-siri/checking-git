2d5e221d82ddc3ab2eddcce360e9f4e3bc3379c5
suvith-siri <77716573+suvith-siri@users.noreply.github.com>
2021-03-28 23:27:39 +0530

    first commit

---
+++ app/src/main/java/com/example/todofinal/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.todofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<String> toDoList;
+    ArrayAdapter<String> arrayAdapter;
+    ListView listView;
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toDoList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this,R.layout.list_view_layout,toDoList);
+        listView = findViewById(R.id.id_list_view);
+
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView textView = (TextView) view;
+                textView.setPaintFlags(textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            }
+        });
+
+        editText = findViewById(R.id.id_edit_text);
+    }
+    public void addItemToList(View view){
+        toDoList.add(editText.getText().toString());
+        arrayAdapter.notifyDataSetChanged();
+
+        editText.setText("");
+    }
+}
